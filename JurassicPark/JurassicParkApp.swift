c49970b97f36cbd2c6d546872704f7aa4c43d00b
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2024-04-23 11:07:05 +0530

    Initial Commit

---
+++ JurassicPark/JurassicParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  JurassicParkApp.swift
+//  JurassicPark
+//
+//  Created by Samrat Mukherjee on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct JurassicParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
